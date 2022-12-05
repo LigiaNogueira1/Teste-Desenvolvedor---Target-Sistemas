1fde3e98b8482d26e2abd6a9d2a455ec9b206bd9
Lígia Nogueira <ligiatht@gmail.com>
2022-12-05 09:42:32 -0300

    Altera resposta da Questão 2

--- Question2.py
+++ Question2.py
@@ -8,21 +8,21 @@ lista = []
 termo1 = int(0)
 termo2 = int(1)
 termo3 = int(0)
+valor = 0
 
 valor = int(input("Digite um número e veja se ele está contido na Sequência de Fibonacci: "))
-if valor == 0 or valor == 1:
-    print ("Digite um número que não seja 0 ou 1:")
 
 while valor > termo3:
-    lista.append(termo3)
     termo3 = termo1 + termo2
     termo1 = termo2
     termo2 = termo3
+    lista.append(termo3)
 
-if valor == termo3:
-    print(lista)
-    if valor in lista:
-        print("O número está contido na Sequência de Fibonacci.")
-    else:
-        print("O número não está contido na Sequência de Fibonacci.")
+print("Confira a lista gerada:", lista)
+if valor == 0 or valor == 1:
+    print("O número está contido na Sequência de Fibonacci.")
+elif valor == termo3:
+    print("O número está contido na Sequência de Fibonacci.")
+else:
+    print("O número não está contido na Sequência de Fibonacci.")
 
