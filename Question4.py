206ccbd6a0786e5d8a401f707a344bca1f6ef8d5
Lígia Nogueira <ligiatht@gmail.com>
2022-12-05 10:32:11 -0300

    Insere enunciado da Questão 4

--- Question4.py
+++ Question4.py
@@ -1,5 +1,14 @@
 #Questão4 - Target Sistemas
 
+#Dado o valor de faturamento mensal de uma distribuidora, detalhado por estado:
+#SP – R$67.836,43 
+#RJ – R$36.678,66 
+#MG – R$29.229,88
+#ES – R$27.165,48 
+#Outros – R$19.849,53
+#Escreva um programa na linguagem que desejar onde calcule o percentual de representação que cada estado teve dentro do valor total mensal da distribuidora.
+
+
 print("-" *42)
 print("Calculando o percentual de representação")
 print("-" *42)
