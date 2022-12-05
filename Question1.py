a2e2c01b75a85fa2613f8810577e24fe18564258
Lígia Nogueira <ligiatht@gmail.com>
2022-12-05 10:27:46 -0300

    Insere enunciado da Questão 1

--- Question1.py
+++ Question1.py
@@ -1,9 +1,14 @@
 #Questão1 - Target Sistemas
 
-#Escreva um programa que inverta os caracteres de um string.
-#IMPORTANTE: 
-#a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código; 
-#b) Evite usar funções prontas, como, por exemplo, reverse.
+#Observe o trecho de código abaixo:
+
+#int INDICE = 13, SOMA = 0, K = 0;
+#enquanto K < INDICE faça {
+#   K = K + 1;
+#   SOMA = SOMA + K;
+#}
+#imprimir(SOMA);
+#Ao final do processamento, qual será o valor da variável SOMA?
 
 
 print("-" *42)
